1e9b2565f6c1bbf1485caeb00c3efecbc912a67e
Ralph Andy Menz <andyralphmenz@gmail.com>
2025-02-20 12:16:39 +0000

    python file

---
+++ src/lessons.py
@@ -0,0 +1,21 @@
+print('Welcome PE 3')import logging
+
+# Create a logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create a file handler and a stream handler
+file_handler = logging.FileHandler('app.log')
+stream_handler = logging.StreamHandler()
+
+# Create a formatter and set it for the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+print('Welcome PE 3')
+logger.info('Application started')
