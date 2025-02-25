9012bc0bcd8bb94f74a6209a79c9b4bf0dd42f8a
Ralph Andy Menz <andyralphmenz@gmail.com>
2025-02-25 23:25:02 +0000

    ...

--- src/components/meme generator/memeImg.js
+++ src/components/meme generator/memeImg.js
@@ -5,7 +5,6 @@ export default function memeimg(){
         <>
         <div className="imgWrapper">
             <  img src={Img} className="memeImg" alt="###"/>
-            <  img src={Img} className="memeImg" alt="###"/>
         </div>
         </>
     )
